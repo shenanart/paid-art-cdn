3aef02dbce42e920e577fd94ac4e1f29acb7ab31
shenanart <shenanart@gmail.com>
2026-03-08 23:41:31 -0400

    Default redirect

--- src/paid_art_cdn/__init__.py
+++ src/paid_art_cdn/__init__.py
@@ -3,13 +3,15 @@ from contextlib import asynccontextmanager
 from typing import cast
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from starlette.types import ExceptionHandler
 
 from .database import init_db
 from .routers.auth_router import router as auth_router
-from .routers.cdn_router import limiter, router as cdn_router
+from .routers.cdn_router import limiter
+from .routers.cdn_router import router as cdn_router
 
 
 @asynccontextmanager
@@ -28,7 +30,14 @@ app = FastAPI(
 
 # Attach the rate limiter and its 429 handler
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, cast(ExceptionHandler, _rate_limit_exceeded_handler))
+app.add_exception_handler(
+    RateLimitExceeded, cast(ExceptionHandler, _rate_limit_exceeded_handler)
+)
 
 app.include_router(auth_router)
 app.include_router(cdn_router)
+
+
+@app.get("/")
+def homepage() -> RedirectResponse:
+    return RedirectResponse("https://shenan.art")
